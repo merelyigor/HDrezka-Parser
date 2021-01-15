5f1f4fd42f4e663dc402ccc919749247fc5614f6
Merely_igor_homePC <mogila1708@gmail.com>
2021-01-16 01:06:35 +0200

    Updated 13

--- parser/serials/pars_serials_urls_by_pagination.php
+++ parser/serials/pars_serials_urls_by_pagination.php
@@ -3,11 +3,11 @@
  * Parser serials url by pagination
  * ---------------------------------------------------------------------------------------------------------------------
  */
-//if (!isset($GLOBALS['proxy_type_global']))
-//    $proxy_type_global = false;
-//
-//$parser_serials_url = "$site_domain_global/{$GLOBALS['serials_slug_parser_global']}";
-//
-//(true) ? $result_global_end_script_pars_films_url = (new ParserHD)->get_media_urls_by_pagination_page($parser_serials_url) : exit('no permission to run the script!');
-//
-//require 'pars_one_serial_data_by_urls_csv.php';
+if (!isset($proxy_type_global))
+    $proxy_type_global = false;
+
+$parser_serials_url = "{$site_domain_global}/{$serials_slug_parser_global}";
+
+//(true) ? $result_global_end_script_pars_serials_url = (new ParserHD)->get_media_urls_by_pagination_page($parser_serials_url) : exit('no permission to run the script!');
+
+require 'pars_one_serial_data_by_urls_csv.php';
