8e985254791d7f0b071c22add0f16f5d7f9a9236
Merely_igor_homePC <mogila1708@gmail.com>
2021-01-16 22:32:35 +0200

    Updated 18 films parser done - TODO serials parser

--- parser/type-parser/films/pars_films_urls_by_pagination.php
+++ parser/type-parser/films/pars_films_urls_by_pagination.php
@@ -8,7 +8,6 @@ if (!isset($GLOBALS['proxy_type_global']))
 
 $parser_films_url = "{$site_domain_global}/{$films_slug_parser_global}";
 
-# можно отключить получение и парсинг урлов фильмов и работать с уже готовым имеющимся файлом (true) просто поменять true на false
 (true) ? $result_global_end_script_pars_films_url = (new Parser)->get_media_urls_by_pagination_page($parser_films_url) : exit('no permission to run the script!');
 
 require_once 'pars_one_film_data_by_urls_csv.php';
