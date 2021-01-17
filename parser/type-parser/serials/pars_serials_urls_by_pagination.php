5281bba7361bbab7bf91051b5346960a8308f98f
Merely_igor_homePC <mogila1708@gmail.com>
2021-01-18 00:26:41 +0200

    Update pars_serials_urls_by_pagination.php

--- parser/type-parser/serials/pars_serials_urls_by_pagination.php
+++ parser/type-parser/serials/pars_serials_urls_by_pagination.php
@@ -8,6 +8,6 @@ if (!isset($proxy_type_global))
 
 $parser_serials_url = "{$site_domain_global}/{$serials_slug_parser_global}";
 
-//(true) ? $result_global_end_script_pars_serials_url = (new Parser)->get_media_urls_by_pagination_page($parser_serials_url) : exit('no permission to run the script!');
+(true) ? $result_global_end_script_pars_serials_url = (new Parser)->get_media_urls_by_pagination_page($parser_serials_url) : exit('no permission to run the script!');
 
 require_once 'pars_one_serial_data_by_urls_csv.php';
